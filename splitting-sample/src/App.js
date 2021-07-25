34b24044e8fe6271e6079f9252a160d70ac31967
kth4249 <desales4249@gmail.com>
2021-07-25 21:29:28 +0900

    loadable component를 사용한 code splitting / preload 방법

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,21 +1,30 @@
-import logo from "./logo.svg";
+import loadable from "@loadable/component";
+import React, { useState } from "react";
 import "./App.css";
-import React, { Suspense, useState } from "react";
-const SplitMe = React.lazy(() => import("./SplitMe"));
+import logo from "./logo.svg";
+// const SplitMe = React.lazy(() => import("./SplitMe"));
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {/* <Suspense fallback={<div>loading...</div>}> */}
+        {visible && <SplitMe />}
+        {/* </Suspense> */}
       </header>
     </div>
   );
